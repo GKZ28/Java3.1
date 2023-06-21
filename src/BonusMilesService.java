f1e404d3fc26b225f92e541ffe413aeab6613107
gkz <giorgadzekonstantin@gmail.com>
2023-06-21 13:50:48 +0400

    Create programm v.1

---
+++ src/BonusMilesService.java
@@ -0,0 +1,17 @@
+public class BonusMilesService {
+
+    public int calculate(int a) {
+        int calculate = a / 20;
+        return calculate;
+    }
+}
+
+
+//public class Main {
+//    public static void main(String[] args){
+//        int x = 13_676;
+//        int y = x / 20;
+//        System.out.println("Количество бонусных миль:");
+//        System.out.println(y);
+//    }
+//}
